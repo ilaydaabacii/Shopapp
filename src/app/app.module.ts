542cb1b73e1f1f9b1769fc1b92117a9a801ec0be
ilaydaabacii <ilaydaabaci05@gmail.com>
2022-06-15 18:39:16 +0300

    added router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,28 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
 
+const appRoutes: Routes= [
+  {path:', component: HomeComponent'},
+  {path: 'home', component: HomeComponent},
+  {path: 'products', component: ProductsComponent},
+  {path:'users', component: UsersComponent},
+  {path:'**', component: NotfoundComponent}
+];
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ProductsComponent,
     UsersComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
